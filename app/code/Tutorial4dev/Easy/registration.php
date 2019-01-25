7aca2936e30611b3e196a9f2b9644573cd5b6465
Zap-Dev <pasquale.guarino80@gmail.com>
2019-01-25 14:53:28 +0100

    Magento 2 - Modulo di Base

---
+++ app/code/Tutorial4dev/Easy/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tutorial4Dev
+ * Date: 17/01/19
+ * Time: 18.42
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Tutorial4dev_Easy',
+    __DIR__
+);
